91cb98c7327ee0d2693f43bb44b153b92ee3b718
shevedasha <165139398+shevedasha@users.noreply.github.com>
2024-09-11 23:56:51 +0500

    Update lesson_2_task_3.py

--- 02_lesson/lesson_2_task_3.py
+++ 02_lesson/lesson_2_task_3.py
@@ -1,9 +1,9 @@
 from math import ceil
-def square(a):
-    s = a ** 2
-    return s
 
-a = float(input("Длина стороны квадрата: "))
-result = square(a)
-rounded_result = ceil(result)
-print(f'Округленная в большую сторону сумма - {rounded_result}')
+
+def square(a):
+    return ceil(a ** 2)
+    
+
+result = square(float(input("Длина стороны квадрата: ")))
+print(f'Округленная в большую сторону сумма - {result}')
