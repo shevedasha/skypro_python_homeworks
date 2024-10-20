9c856eec6b1e75c6cabc44842343ace3cbd5a93f
shevedasha <shevedasha@ya.ru>
2024-10-20 15:14:30 +0500

    09_lesson_prsvki

--- 09_lesson/test_dz9.py
+++ 09_lesson/test_dz9.py
@@ -1,6 +1,4 @@
 import pytest
-import requests
-
 from sqlalchemy import create_engine
 from sqlalchemy import text
 
@@ -11,24 +9,22 @@ def test_select():
     db = create_engine(db_connection_string)
     rows = db.execute("select * from subject").fetchall()
     row1 = rows[0]
-
-assert row1["subject_title"] == 'English'
+    assert row1["subject_title"] == 'English'
 
 def test_insert():
 
     db = create_engine(db_connection_string)
-    db.execute('''INSERT into subject (subject_id, subject_title) VALUES(17, 'Alchemy');''')
+    db.execute('''INSERT into subject (subject_id, subject_title) VALUES (17, 'Alchemy');''')
     rows = db.execute("select * from subject").fetchall()
-    row_1=rows[-1]
+    row1=rows[-1]
     sql = text("DELETE FROM subject WHERE subject_id = :id")
     db.execute(sql, id=17)
-
-assert row1["subject_title"] == 'Alchemy'
+    assert row1["subject_title"] == 'Alchemy'
 
 def test_update():
  
     db = create_engine(db_connection_string)
-    db.execute('''INSERT into subject (subject_id, subject_title) VALUES(17, 'Alchemy');''')
+    db.execute('''INSERT into subject (subject_id, subject_title) VALUES (17, 'Alchemy');''')
     rows = db.execute("select * from subject").fetchall()
     row1=rows[-1]
     sql = text("update subject set subject_title = :new_title where subject_id=:id")
@@ -38,16 +34,13 @@ def test_update():
     sql = text("DELETE FROM subject WHERE subject_id = :id")
     id_update = row2["subject_id"]
     db.execute(sql, id=id_update)
-
-assert rows["subject_title"] == "Alchemy2"
+    assert row2["subject_title"] == "Alchemy2"
 
 def test_delete():
 
     db = create_engine(db_connection_string)
-    db.execute('''INSERT into subject (subject_id, subject_title) VALUES(17, 'Alchemy');''')
+    db.execute('''INSERT into subject (subject_id, subject_title) VALUES (17, 'Alchemy');''')
     rows = db.execute("select * from subject").fetchall()
     row1=rows[-1]
     sql = text("DELETE FROM subject WHERE subject_id = :id")
     db.execute(sql, id=17)
-
-assert row1["name"] == name
